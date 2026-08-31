fe9ac0c7e4889e3e9e0ad59d03b392de8783ecf5
Denis <mynameisdavis1997@gmail.com>
2026-08-31 15:55:09 +0300

    fix(xconnect): provide cqrs dependencies for auth guard

--- src/modules/xconnect-user-preferences/xconnect-user-preferences.module.ts
+++ src/modules/xconnect-user-preferences/xconnect-user-preferences.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 import { XConnectUserPreferencesController } from './xconnect-user-preferences.controller';
 import { XConnectUserPreferencesService } from './xconnect-user-preferences.service';
 
 @Module({
+    imports: [CqrsModule],
     controllers: [XConnectUserPreferencesController],
     providers: [XConnectUserPreferencesService],
     exports: [XConnectUserPreferencesService],
